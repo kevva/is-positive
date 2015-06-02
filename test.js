1187a61f2e18cf7c11c23d61a1bd52b9fa6a5fdf
Kevin Martensson <kevinmartensson@gmail.com>
2015-06-02 14:01:52 +0200

    Initial commit

---
+++ test.js
@@ -0,0 +1,10 @@
+'use strict';
+var test = require('ava');
+var isPositive = require('./');
+
+test(function (t) {
+	t.assert(isPositive(1));
+	t.assert(isPositive(0));
+	t.assert(!isPositive(-1));
+	t.end();
+});
