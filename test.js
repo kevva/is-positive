016e6676f02e74c2f043386d9177cec5b8d3516e
Sorin Iclanzan <sorin@iclanzan.com>
2015-06-18 19:22:08 +0300

    Test for +0 and -0.

--- test.js
+++ test.js
@@ -4,7 +4,8 @@ var isPositive = require('./');
 
 test(function (t) {
 	t.assert(isPositive(1));
-	t.assert(!isPositive(0));
+	t.assert(isPositive(0));
+	t.assert(!isPositive(-0));
 	t.assert(!isPositive(-1));
 	t.end();
 });
