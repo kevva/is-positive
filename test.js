97edff6f525f192a3f83cea1944765f769ae2678
Robert Penner <robert@robertpenner.com>
2016-03-23 22:22:56 +0545

    Improve tests
    Fixes #9.

--- test.js
+++ test.js
@@ -1,10 +1,14 @@
+/* eslint no-new-wrappers: 0 */
 import test from 'ava';
 import m from './';
 
 test(t => {
 	t.true(m(1));
+	t.true(m(new Number(1)));
 	t.false(m(0));
 	t.false(m(-1));
 	t.false(m('1'));
-	t.true(m(Number(1)));
+	t.false(m(NaN));
+	t.false(m(true));
+	t.false(m());
 });
