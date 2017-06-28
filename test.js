29048c5a79e04121fd44e8a8a26daceca3df5412
Brandon Newton <btnwtn@gmail.com>
2017-06-28 09:38:49 -0700

    Update tests to reflect Type Coercion.

--- test.js
+++ test.js
@@ -7,8 +7,8 @@ test(t => {
 	t.true(m(new Number(1)));
 	t.false(m(0));
 	t.false(m(-1));
-	t.false(m('1'));
+	t.true(m('1'));
 	t.false(m(NaN));
-	t.false(m(true));
+	t.true(m(true));
 	t.false(m());
 });
